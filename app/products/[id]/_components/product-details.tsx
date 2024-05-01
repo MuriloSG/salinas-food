cc71c89b4fbc28dcd1f7d84fea7c80e5c7a3fbbf
MuriloSG <muriloprojetosti@gmail.com>
2024-05-01 20:18:16 -0300

    feat: add restaurant page

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -7,15 +7,10 @@ import {
   formatCurrency,
 } from "@/app/_helpers/price";
 import { Button } from "@/app/_components/ui/button";
-import {
-  BikeIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  TimerIcon,
-} from "lucide-react";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { useState } from "react";
-import { Card } from "@/app/_components/ui/card";
 import ProductList from "@/app/_components/product-list";
+import DeliveryInfo from "@/app/_components/delivery-info";
 
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{
@@ -97,34 +92,7 @@ const ProductDetails = ({
       </div>
       {/*DADOS DA ENTREGA*/}
       <div className="px-5">
-        <Card className="mt-6 flex justify-around py-3">
-          {/*CUSTO*/}
-          <div className="flex flex-col items-center">
-            <div className="flex items-center gap-1 text-muted-foreground">
-              <span className="text-xs">Entrega</span>
-              <BikeIcon size={14} />
-            </div>
-
-            {Number(product.restaurant.deliveryFee) > 0 ? (
-              <p className=" text-xs font-semibold">
-                {formatCurrency(Number(product.restaurant.deliveryFee))}
-              </p>
-            ) : (
-              <p className=" text-xs font-semibold">Grátis</p>
-            )}
-          </div>
-          {/*TEMPO*/}
-          <div className="flex flex-col items-center">
-            <div className="flex items-center gap-1 text-muted-foreground">
-              <span className="text-xs">Tempo de Entrega</span>
-              <TimerIcon size={14} />
-            </div>
-
-            <p className=" text-xs font-semibold">
-              {product.restaurant.deliveryTimeMinutes}min
-            </p>
-          </div>
-        </Card>
+        <DeliveryInfo restaurant={product.restaurant} />
       </div>
 
       <div className="mt-6 space-y-3 px-5">
