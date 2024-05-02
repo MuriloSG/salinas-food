f2b97d3d3f7ed1d0d427155dea353345be8f0c01
MuriloSG <muriloprojetosti@gmail.com>
2024-05-02 19:27:18 -0300

    fix: correction operation deleveryFee in total price cart

--- app/_context/Cart.tsx
+++ app/_context/Cart.tsx
@@ -68,7 +68,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     return (
       products.reduce((acc, product) => {
         return acc + calculateProductTotalPrice(product) * product.quantity;
-      }, 0) - Number(products?.[0]?.restaurant?.deliveryFee)
+      }, 0) + Number(products?.[0]?.restaurant?.deliveryFee)
     );
   }, [products]);
 
